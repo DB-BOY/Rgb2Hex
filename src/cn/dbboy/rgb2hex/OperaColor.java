542f1825a82ade7eebb7335b1e6e3d1d9386ea38
DB_BOY <ginngi_wang@163.com>
2019-10-08 12:08:32 +0800

    添加alpha百分比计算

--- src/cn/dbboy/rgb2hex/OperaColor.java
+++ src/cn/dbboy/rgb2hex/OperaColor.java
@@ -20,15 +20,20 @@ public class OperaColor extends Color {
     } 
   
     private static String toBrowserHexValue(int number) {  
-        StringBuilder builder = new StringBuilder(  
-                Integer.toHexString(number & 0xff));  
-        while (builder.length() < 2) {  
-            builder.append("0");  
-        }  
-        return builder.toString().toUpperCase();  
+        StringBuilder builder = new StringBuilder(Integer.toHexString(number & 0xff));
+        if(builder.length()<2){
+            while (builder.length() < 2) {
+                builder.append("0");
+            }
+            builder.reverse();
+        }
+        return builder.toString().toUpperCase();
     }  
     public static void main(String[] args) {  
-        System.out.println(toHex(22,112,160,219));  
-        System.out.println(toHex(112,160,219));  
-    }  
+        System.out.println(toHex(22,11,160,219));
+        System.out.println(toHex(112,160,219));
+        for (int i = 0; i < 256; i++) {
+            System.out.println(toBrowserHexValue(i));
+        }
+    }
 }  
--- src/cn/dbboy/rgb2hex/Rgb2Hex.java
+++ src/cn/dbboy/rgb2hex/Rgb2Hex.java
@@ -1,199 +1,270 @@
 package cn.dbboy.rgb2hex;
 
-import java.awt.Color;
-import java.awt.FlowLayout;
+import java.awt.*;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 
-import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
-import javax.swing.JTextField;
 import javax.swing.border.EmptyBorder;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
 
 public class Rgb2Hex {
 
-	public static void main(String[] args) {
-		Rgb2Hex r = new Rgb2Hex();
-		r.createView();
-	}
+    public static void main(String[] args) {
+        Rgb2Hex r = new Rgb2Hex();
+        r.createView();
+    }
 
-	JFrame f;
-	JTextArea tAlpha;
-	JTextArea tRed;
-	JTextArea tGreen;
-	JTextArea tBlue;
-	JTextArea tfHex;
-	JLabel left,right,dot1,dot2,dot3;
+    JFrame f;
+    JTextArea numAlpha;
+    JTextArea numRed;
+    JTextArea numGreen;
+    JTextArea numBlue;
+    JTextArea hexArea;
+    JTextArea alphaP;
+    JLabel leftLabel, rightLabel, dot1Label, dot2Label, dot3Label, alphaLabel;
+    JPanel hexPanel, numPanel, alphaPanel,colorPanel;
 
-	private void createView() {
-		f = new JFrame("Rgb2Hex");
-		f.setSize(400, 300);
-		f.setLocation(200, 200);
-		f.setLayout(null);
+    private void createView() {
+        f = new JFrame("Rgb2Hex");
+        f.setSize(400, 500);
+        f.setLocation(300, 200);
+        f.setLayout(null);
 
-		JPanel p1 = new JPanel();
-		p1.setBounds(50, 50, 300, 60);
-		// 输入框
-		tfHex = new JTextArea("#ffffff");
-		tfHex.setFont(new java.awt.Font("Default", 0, 25));
-		tfHex.setForeground(Color.GRAY);
-		tfHex.setOpaque(false); // 把panel的背景色设为透明
-		tfHex.setEditable(false);
-		tfHex.setBorder(new EmptyBorder(8, 8, 8, 8));
-		p1.add(tfHex);
+        hexPanel = createPanel();
+        hexPanel.setBounds(50, 10, 180, 50);
+        // 输入框
+        hexArea = new JTextArea("#ffffff");
+        hexArea.setFont(new java.awt.Font("Default", 0, 25));
+        hexArea.setForeground(Color.black);
+        hexArea.setEditable(false);
+        hexArea.setBorder(new EmptyBorder(8, 8, 8, 8));
+        hexPanel.add(hexArea);
 
-		JPanel p2 = new JPanel();
-		p2.setBounds(10, 150, 300, 60);
+        //alpha百分比计算
+        alphaPanel = createPanel();
+        alphaPanel.setBounds(50, 70, 120, 32);
+        alphaLabel = createLabel("Alpha百分比");
 
-		 left = new JLabel("argb(");
-		// 把按钮加入面板
-		p2.add(left);
-		// alpha
-		tAlpha = createTextField("255");
-		p2.add(tAlpha);
+        alphaPanel.add(alphaLabel);
+        alphaP = createAlphaText("100");
+        alphaPanel.add(alphaP);
 
-		dot1 = new JLabel(",");
-		p2.add(dot1);
+        //数字盘
+        numPanel = createPanel();
+        numPanel.setBounds(50, 100, 260, 32);
+        leftLabel = createLabel("argb(");
+        // 把按钮加入面板
+        numPanel.add(leftLabel);
+        // alpha
+        numAlpha = createTextField("255");
+        numPanel.add(numAlpha);
 
-		// red
-		tRed = createTextField("255");
-		p2.add(tRed);
+        dot1Label = createLabel(",");
+        numPanel.add(dot1Label);
 
-		dot2 = new JLabel(",");
-		p2.add(dot2);
+        // red
+        numRed = createTextField("255");
+        numPanel.add(numRed);
 
-		// green
-		tGreen = createTextField("255");
-		p2.add(tGreen);
+        dot2Label = createLabel(",");
+        numPanel.add(dot2Label);
 
-		dot3 = new JLabel(",");
-		p2.add(dot3);
+        // green
+        numGreen = createTextField("255");
+        numPanel.add(numGreen);
 
-		// blue
-		tBlue = createTextField("255");
-		p2.add(tBlue);
+        dot3Label = createLabel(",");
+        numPanel.add(dot3Label);
 
-		right = new JLabel(");");
-		p2.add(right);
+        // blue
+        numBlue = createTextField("255");
+        numPanel.add(numBlue);
 
-		p1.setBackground(Color.white);
-		p1.setOpaque(false); // 把panel的背景色设为透明
-		p2.setBackground(Color.BLUE);
-		p2.setOpaque(false); // 把panel的背景色设为透明
+        rightLabel = createLabel(");");
+        numPanel.add(rightLabel);
 
-		// 设置面板背景颜色
 
-		setBackGround();
-		// 把面板加入窗口
-		f.add(p1);
-		f.add(p2);
-		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		f.setVisible(true);
-	}
+		colorPanel = createPanel();
+		colorPanel.setBounds(0, 132,400,500);
+		colorPanel.setOpaque(true);
 
-	private JTextArea createTextField(String name) {
-		JTextArea tBlue = new JTextArea(name);
-		tBlue.setFont(new java.awt.Font("Default", 0, 13));
-		tBlue.setForeground(Color.GRAY);
-		tBlue.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setListener(tBlue);
-		tBlue.setOpaque(false); // 把panel的背景色设为透明
-		return tBlue;
-	}
+        setBackGround();
+        // 把面板加入窗口
+        f.add(hexPanel);
+        f.add(numPanel);
+        f.add(alphaPanel);
+        f.add(colorPanel);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.getContentPane().setBackground(Color.white);
+        f.setVisible(true);
+    }
 
-	private void setBackGround() {
-		String r = tRed.getText().trim();
-		String a = tAlpha.getText().trim();
-		String g = tGreen.getText().trim();
-		String b = tBlue.getText().trim();
+    private JPanel createPanel() {
+        JPanel panel = new JPanel();
+        panel.setOpaque(false); // 把panel的背景色设为透明
+        return panel;
+    }
 
-		System.out.println(a + " " + r + " " + g + " " + b);
-		int ca = getColor(a);
-		int cr = getColor(r);
-		int cg = getColor(g);
-		int cb = getColor(b);
+    private JLabel createLabel(String s) {
+        JLabel label = new JLabel(s);
+        label.setForeground(Color.black);
+        return label;
+    }
 
-		System.out.println(ca + " " + cr + " " + cg + " " + cb);
-		Color c = new Color(cr, cg, cb, ca);
+    private JTextArea createAlphaText(String name) {
+        JTextArea textArea = new JTextArea(name);
+        textArea.setFont(new java.awt.Font("Default", 0, 13));
+        textArea.setForeground(Color.black);
+        textArea.setBorder(new EmptyBorder(5, 5, 5, 5));
+        textArea.setOpaque(false); // 把panel的背景色设为透明
+        textArea.getDocument().addDocumentListener(new DocumentListener() {
 
-		f.getContentPane().setBackground(c);
-		
-		c = new Color(255-cr,255-cg,255-cb);
-		tfHex.setForeground(c);
-		tfHex.setText(OperaColor.toHex(ca, cr, cg, cb));
-		
-		tRed.setForeground(c);
-		tBlue.setForeground(c);
-		tGreen.setForeground(c);
-		tAlpha.setForeground(c);
-		
-		left.setForeground(c);
-		right.setForeground(c);
-		dot1.setForeground(c);
-		dot2.setForeground(c);
-		dot3.setForeground(c);
-		
-	}
+            @Override
+            public void removeUpdate(DocumentEvent e) {
+                System.out.println("alpha-------remove " + textArea.getText());
+                setAlpha(textArea.getText());
+            }
 
-	private int getColor(String color) {
-		int result = 255;
-		try{
-			result = Integer.valueOf(color);	
-		}catch(NumberFormatException e){
-			result =255;
-		}
-		if(result>255){
-			result=255;
-		}
-		return result;
-	}
+            @Override
+            public void insertUpdate(DocumentEvent e) {
+                String x = textArea.getText();
+                if (x.length() > 3) {
+                    return;
+                }
+                setAlpha(x);
+                System.out.println("alpha-------insert " + x);
+            }
 
-	private void setListener(JTextArea text) {
-		text.getDocument().addDocumentListener(new DocumentListener() {
+            @Override
+            public void changedUpdate(DocumentEvent e) {
+                System.out.println("alpha-------rchanged " + textArea.getText());
+                setAlpha(textArea.getText());
+            }
+        });
+        textArea.addKeyListener(new KeyAdapter() {
+            public void keyTyped(KeyEvent e) {
+                int keyChar = e.getKeyChar();
+                if (keyChar >= KeyEvent.VK_0 && keyChar <= KeyEvent.VK_9) {
+                    System.out.println(textArea.getText());
+                    if (textArea.getText().length() >= 3) {
+                        textArea.setText(textArea.getText().substring(0, 2));
+                    }
+                } else {
+                    e.consume(); // 关键，屏蔽掉非法输入
+                }
+            }
+        });
+        return textArea;
+    }
 
-			@Override
-			public void removeUpdate(DocumentEvent e) {
-				System.out.println("remove " + text.getText());
+    private void setAlpha(String text) {
+        System.out.println(text);
+        if (text == null || "".equals(text)) {
+            return;
+        }
+        int number = Integer.valueOf(text);
+        int tmp = (int) (number * 1f / 100 * 255);
+        numAlpha.setText(String.valueOf(tmp));
+    }
 
-				setBackGround();
-			}
+    private JTextArea createTextField(String name) {
+        JTextArea textArea = new JTextArea(name);
+        textArea.setFont(new java.awt.Font("Default", 0, 13));
+        textArea.setForeground(Color.black);
+        textArea.setBorder(new EmptyBorder(5, 5, 5, 5));
+        setListener(textArea);
+        textArea.setOpaque(false);
+        return textArea;
+    }
 
-			@Override
-			public void insertUpdate(DocumentEvent e) {
-				String x = text.getText();
-				if(x.length()>3){
-					return;
-				}
-				setBackGround();
-				System.out.println("insert " + x);
-			}
+    /**
+     * 设置背景颜色
+     */
+    private void setBackGround() {
+        String r = numRed.getText().trim();
+        String a = numAlpha.getText().trim();
+        String g = numGreen.getText().trim();
+        String b = numBlue.getText().trim();
 
-			@Override
-			public void changedUpdate(DocumentEvent e) {
-				System.out.println("changed " + text.getText());
+        System.out.println("before_____" + a + " " + r + " " + g + " " + b);
+        int ca = getColor(a);
+        int cr = getColor(r);
+        int cg = getColor(g);
+        int cb = getColor(b);
 
-				setBackGround();
-			}
-		});
-		text.addKeyListener(new KeyAdapter() {
-			public void keyTyped(KeyEvent e) {
-				int keyChar = e.getKeyChar();
-				if (keyChar >= KeyEvent.VK_0 && keyChar <= KeyEvent.VK_9) {
-					System.out.println(text.getText());
-					if(text.getText().length()>=3){
-						text.setText(text.getText().substring(0,2));
-					}
-				} else {
-					e.consume(); // 关键，屏蔽掉非法输入
-				}
-			}
-		});
-		// text.setDocument(new NumberLenghtLimitedDmt(3));
-	}
+        System.out.println("after_____" + ca + " " + cr + " " + cg + " " + cb);
+        //RGBA
+        Color c = new Color(cr, cg, cb, ca);
+
+		colorPanel.setBackground(c);
+        hexArea.setText(OperaColor.toHex(ca, cr, cg, cb));
+//		c = new Color(255 - cr, 255 - cg, 255 - cb);
+//		hexArea.setForeground(c);
+
+//		numRed.setForeground(c);
+//        numBlue.setForeground(c);
+//        numGreen.setForeground(c);
+//        numAlpha.setForeground(c);
+//        alphaP.setForeground(c);
+    }
+
+    private int getColor(String color) {
+        int result = 255;
+        try {
+            result = Integer.valueOf(color);
+        } catch (NumberFormatException e) {
+            result = 255;
+        }
+        if (result > 255) {
+            result = 255;
+        }
+        return result;
+    }
+
+    private void setListener(JTextArea text) {
+        text.getDocument().addDocumentListener(new DocumentListener() {
+
+            @Override
+            public void removeUpdate(DocumentEvent e) {
+                System.out.println("remove " + text.getText());
+                setBackGround();
+            }
+
+            @Override
+            public void insertUpdate(DocumentEvent e) {
+                String x = text.getText();
+                if (x.length() > 3) {
+                    return;
+                }
+                setBackGround();
+                System.out.println("insert " + x);
+            }
+
+            @Override
+            public void changedUpdate(DocumentEvent e) {
+                System.out.println("changed " + text.getText());
+
+                setBackGround();
+            }
+        });
+        text.addKeyListener(new KeyAdapter() {
+            public void keyTyped(KeyEvent e) {
+                int keyChar = e.getKeyChar();
+                if (keyChar >= KeyEvent.VK_0 && keyChar <= KeyEvent.VK_9) {
+                    System.out.println(text.getText());
+                    if (text.getText().length() >= 3) {
+                        text.setText(text.getText().substring(0, 2));
+                    }
+                } else {
+                    e.consume(); // 关键，屏蔽掉非法输入
+                }
+            }
+        });
+    }
 
 }
